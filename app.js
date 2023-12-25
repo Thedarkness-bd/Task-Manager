fe604bc1a44803678270e5c5083e0ed7671fae7f
Thedarkness-bd <toisiftasrik@gmail.com>
2023-12-25 20:30:30 +0600

    get all routes created and controller folder created

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/Tasks");
+app.use(express.json());
 
-app.get("/hello", (req,res) => {
+//routes
+app.get("/hello", (req, res) => {
   res.send("Task manager app");
 });
 
-
-
-
+app.use("/api/v1/tasks", tasks);
 
 const PORT = 3000;
 app.listen(PORT, console.log(`Listening on port ${PORT}`));
